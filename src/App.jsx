3dfd7de44c46236b10bdae94f136a63e9c9d8fcb
Nelson Wu <Nelsonwu820@gmail.com>
2024-06-06 14:15:16 -0400

    added location serach bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import LocationInput from './components/LocationInput.jsx';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='app_parent'>
+      <LocationInput/>
+    </section>
   )
 }
 
