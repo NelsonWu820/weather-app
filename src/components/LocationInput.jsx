6972e3e9d13446e4fd69b325939c70e381088245
Nelson Wu <Nelsonwu820@gmail.com>
2024-06-06 18:39:43 -0400

    adding the google maps location input field

--- src/components/LocationInput.jsx
+++ src/components/LocationInput.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import { StandaloneSearchBox, LoadScript } from '@react-google-maps/api';
 
+//Here I'm calling google maps Javascript Maps API through ract-google-maps/ api and not doing it async
+//because I can't then call Open weather without lat & long so it dosen't need the async
 const LocationInput = () => {
+  const inputRef = useRef();
+
+  const placeChanged = () => {
+    const [place] = inputRef.current.getPlaces();
+    if(place){
+      console.log(place.formatted_address)
+      console.log(place.geometry.location.lat());
+      console.log(place.geometry.location.lng());
+    }
+  };
+  
+
   return (
     <div>
-      Hello
+      <LoadScript 
+        googleMapsApiKey="AIzaSyC8d3-x0XMeyyBqDPhnkcYbqdyur6vpYgo"
+        libraries={["places"]}
+      >
+        <StandaloneSearchBox
+          onLoad={ref => (inputRef.current = ref)}
+          onPlacesChanged={placeChanged}
+        >
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Location"
+          />
+
+        </StandaloneSearchBox>
+      </LoadScript>
     </div>
   )
 }
