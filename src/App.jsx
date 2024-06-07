1d758b2f0bcc70250ccbcc76fdcab480f4141ae0
Nelson Wu <Nelsonwu820@gmail.com>
2024-06-07 07:41:01 -0400

    made cords to seperate lat and long states

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-require('dotenv').config;
 import LocationInput from './components/LocationInput.jsx';
 
 const App = () => {
   //holds the coordinates i,e lat & long
-  const [cords, setCords] = useState([]);
+  const [lat, setLat] = useState();
+  const [long, setLong] = useState();
   //holds all the json from OpenWeather
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   //I could put the axios after setCords of newLocation but this is incase of an edge case where the user somehow changes the cords
   useEffect(() => {
-    //also calls OpenWeather to get json then updates 
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cords[0]}&lon=${cords[1]}&appid=`)
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, [cords]);
+    if (lat !== undefined && long !== undefined){
+      //also calls OpenWeather to get json then updates 
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
+        .then(response => {
+          setData(response.data);
+          console.log(data)
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [lat, long]);
 
   //will be called and update state when the location changes
   const newLocation = (lat, long) => {
-    setCords([lat, long])
-    console.log(cords[0] + "&" + cords[1])
+    //setLat & setLong will auto get rid of old data and replace with new data
+    //if you place console log on lat & long for 2 dif points inside the same state they might look the same at a glance but the decimal points are different
+    setLat(lat)
+    setLong(long)
   };
 
   return (
